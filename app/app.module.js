a4c4c6f99e05d651600379a9fcfdfa5ae5fd2561
Uri Shmueli <Ushmueli@panaya.com>
2018-04-10 18:44:13 +0300

    PhonecatApp module

--- app/app.module.js
+++ app/app.module.js
@@ -1,18 +1,6 @@
-import { bundle } from './panaya-decorators/panaya-decorators';
-import { AppModule } from './next-gen/app-module'
-import 'angular';
-import 'angular-animate';
-import 'angular-route';
-import 'angular-resource';
-
-'use strict';
-
-// Define the `phonecatApp` module
-angular.module('phonecatApp', [
-  'ngAnimate',
-  'ngRoute',
-  'ngResource',
-  bundle(AppModule).name
-]);
-
-require('./app.config');
+import "angular";
+import "angular-animate";
+import "angular-route";
+import "angular-resource";
+import "./phonecatApp.module";
+import "./app.config";
---
+++ app/phonecatApp.module.js
@@ -0,0 +1,12 @@
+import { bundle } from './panaya-decorators/panaya-decorators';
+import { AppModule } from './next-gen/app-module'
+
+'use strict';
+
+// Define the `phonecatApp` module
+angular.module('phonecatApp', [
+  'ngAnimate',
+  'ngRoute',
+  'ngResource',
+  bundle(AppModule).name
+]);
